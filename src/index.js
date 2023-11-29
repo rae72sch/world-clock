032a2cbbcef87621695fc93f7be0c642cb4485d7
Rachel Schofield <rae72sch@hotmail.com>
2023-11-29 13:21:14 +0000

    All three default cities now have live data from Moment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Dublin
 function updateDublinTime() {
   let dublinElement = document.querySelector("#dublin");
   let dublinDateElement = document.querySelector("#dublin .date");
@@ -13,4 +14,38 @@ function updateDublinTime() {
 updateDublinTime();
 setInterval(updateDublinTime, 1000);
 
+// Chicago
+function updateChicagoTime() {
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = document.querySelector("#chicago .date");
+  let chicagoTimeElement = document.querySelector("#chicago .time");
+
+  let chicagoData = moment().tz("America/Chicago");
+
+  chicagoDateElement.innerHTML = chicagoData.format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = chicagoData.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateChicagoTime();
+setInterval(updateChicagoTime, 1000);
+
+
+// Perth
+function updatePerthTime() {
+  let perthElement = document.querySelector("#perth");
+  let perthDateElement = document.querySelector("#perth .date");
+  let perthTimeElement = document.querySelector("#perth .time");
+
+  let perthData = moment().tz("Australia/Perth");
+
+  perthDateElement.innerHTML = perthData.format("MMMM Do YYYY");
+  perthTimeElement.innerHTML = perthData.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updatePerthTime();
+setInterval(updatePerthTime, 1000);
 
