39a247a2ce662795f91d2402c7894be16e0a7b09
Rachel Schofield <rae72sch@hotmail.com>
2023-11-29 06:52:52 +0000

    Added JavaScript to first city to use current timezone time and date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function updateDublinTime() {
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = document.querySelector("#dublin .date");
+  let dublinTimeElement = document.querySelector("#dublin .time");
+
+  let dublinData = moment().tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinData.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinData.format(
+    "h:mm:ss [<small>]A[</small>]");
+}
+
+updateDublinTime();
+setInterval(updateDublinTime, 1000);
+
+
