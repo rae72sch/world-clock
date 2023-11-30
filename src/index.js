1ab45e198df65d7a86c1a977da426c867f3341a0
Rachel Schofield <rae72sch@hotmail.com>
2023-11-30 12:12:53 +0000

    updated in prep for adding functionality to the city selector
    Added all default cities into a single function called updateTime, as well as checking for each of the default cities before trying to update the seconds.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,47 @@
 // Dublin
-function updateDublinTime() {
+function updateTime() {
+  // Dublin
   let dublinElement = document.querySelector("#dublin");
-  let dublinDateElement = document.querySelector("#dublin .date");
-  let dublinTimeElement = document.querySelector("#dublin .time");
+  if (dublinElement) {
+    let dublinDateElement = document.querySelector("#dublin .date");
+    let dublinTimeElement = document.querySelector("#dublin .time");
 
-  let dublinData = moment().tz("Europe/Dublin");
+    let dublinData = moment().tz("Europe/Dublin");
 
-  dublinDateElement.innerHTML = dublinData.format("MMMM Do YYYY");
-  dublinTimeElement.innerHTML = dublinData.format(
-    "h:mm:ss [<small>]A[</small>]");
-}
+    dublinDateElement.innerHTML = dublinData.format("MMMM Do YYYY");
+    dublinTimeElement.innerHTML = dublinData.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-updateDublinTime();
-setInterval(updateDublinTime, 1000);
-
-// Chicago
-function updateChicagoTime() {
+  // Chicago
   let chicagoElement = document.querySelector("#chicago");
-  let chicagoDateElement = document.querySelector("#chicago .date");
-  let chicagoTimeElement = document.querySelector("#chicago .time");
+  if (chicagoElement) {
+    let chicagoDateElement = document.querySelector("#chicago .date");
+    let chicagoTimeElement = document.querySelector("#chicago .time");
 
-  let chicagoData = moment().tz("America/Chicago");
+    let chicagoData = moment().tz("America/Chicago");
 
-  chicagoDateElement.innerHTML = chicagoData.format("MMMM Do YYYY");
-  chicagoTimeElement.innerHTML = chicagoData.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-}
+    chicagoDateElement.innerHTML = chicagoData.format("MMMM Do YYYY");
+    chicagoTimeElement.innerHTML = chicagoData.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-updateChicagoTime();
-setInterval(updateChicagoTime, 1000);
-
-
-// Perth
-function updatePerthTime() {
+  // Perth
   let perthElement = document.querySelector("#perth");
-  let perthDateElement = document.querySelector("#perth .date");
-  let perthTimeElement = document.querySelector("#perth .time");
+  if (perthElement) {
+    let perthDateElement = document.querySelector("#perth .date");
+    let perthTimeElement = document.querySelector("#perth .time");
 
-  let perthData = moment().tz("Australia/Perth");
+    let perthData = moment().tz("Australia/Perth");
 
-  perthDateElement.innerHTML = perthData.format("MMMM Do YYYY");
-  perthTimeElement.innerHTML = perthData.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    perthDateElement.innerHTML = perthData.format("MMMM Do YYYY");
+    perthTimeElement.innerHTML = perthData.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
-updatePerthTime();
-setInterval(updatePerthTime, 1000);
-
+updateTime();
+setInterval(updateTime, 1000);
