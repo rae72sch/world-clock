834601be9c51e1955af55774ec2b6c0955240824
Rachel Schofield <rae72sch@hotmail.com>
2023-12-05 09:53:58 +0000

    Added functionality to show current timezone & difference in hours from current

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 // Dublin
 function updateTime() {
-  // Dublin
-  let dublinElement = document.querySelector("#dublin");
-  if (dublinElement) {
-    let dublinDateElement = document.querySelector("#dublin .date");
-    let dublinTimeElement = document.querySelector("#dublin .time");
+  // Current Timezone
+  let currentElement = document.querySelector("#current");
+  if (currentElement) {
+    let currentTimezoneElement = document.querySelector("#current-tz");
+    let currentDateElement = document.querySelector("#current .date");
+    let currentTimeElement = document.querySelector("#current .time");
 
-    let dublinData = moment().tz("Europe/Dublin");
+    // let dublinData = moment().tz("Europe/Dublin");
+    let currentTimezone = moment.tz.guess();
+    let currentData = moment().tz(currentTimezone);
 
-    dublinDateElement.innerHTML = dublinData.format("MMMM Do YYYY");
-    dublinTimeElement.innerHTML = dublinData.format(
+    currentTimezoneElement.innerHTML = `Current Timezone <br><small>(${currentTimezone})</small>`;
+    currentDateElement.innerHTML = currentData.format("MMMM Do YYYY");
+    currentTimeElement.innerHTML = currentData.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
@@ -19,13 +23,19 @@ function updateTime() {
   if (chicagoElement) {
     let chicagoDateElement = document.querySelector("#chicago .date");
     let chicagoTimeElement = document.querySelector("#chicago .time");
+    let chicagoDifferenceElement = document.querySelector(
+      "#tz-difference-chicago"
+    );
 
     let chicagoData = moment().tz("America/Chicago");
+
+    let timeDifferenceChicago = timezoneDiff("America/Chicago");
 
     chicagoDateElement.innerHTML = chicagoData.format("MMMM Do YYYY");
     chicagoTimeElement.innerHTML = chicagoData.format(
       "h:mm:ss [<small>]A[</small>]"
     );
+    chicagoDifferenceElement.innerHTML = `Difference from current timezone: ${timeDifferenceChicago} hours`;
   }
 
   // Perth
@@ -33,16 +43,34 @@ function updateTime() {
   if (perthElement) {
     let perthDateElement = document.querySelector("#perth .date");
     let perthTimeElement = document.querySelector("#perth .time");
+    let perthDifferenceElement = document.querySelector("#tz-difference-perth");
 
     let perthData = moment().tz("Australia/Perth");
+
+    let timeDifferencePerth = timezoneDiff("Australia/Perth");
 
     perthDateElement.innerHTML = perthData.format("MMMM Do YYYY");
     perthTimeElement.innerHTML = perthData.format(
       "h:mm:ss [<small>]A[</small>]"
     );
+    perthDifferenceElement.innerHTML = `Difference from current timezone: ${timeDifferencePerth} hours`;
   }
 }
 
+// Calculate the difference between the current timezone and the displayed timezone(s)
+function timezoneDiff(searchTimezone) {
+  let localTimezone = moment.tz.guess();
+  let localTime = moment().tz(localTimezone);
+  let localOffset = localTime.utcOffset();
+  console.log(localOffset);
+  let searchCityTime = moment().tz(searchTimezone);
+  let searchCityOffset = searchCityTime.utcOffset();
+  let diffInMinutes = searchCityOffset - localOffset;
+  let diffInHours = diffInMinutes / 60;
+  return diffInHours;
+}
+
+// Update the displayed city when a user selects a city
 function updateCity(event) {
   let cityTimezone = event.target.value;
   if (cityTimezone === "current") {
@@ -51,14 +79,25 @@ function updateCity(event) {
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let citiesElement = document.querySelector("#cities");
+  let timeDifference = timezoneDiff(cityTimezone);
   citiesElement.innerHTML = `
+  <div class="city" id="current">
+    <div>
+      <h2 id="current-tz"></h2>
+      <div class="date"></div>
+    </div>
+    <div class="time"><small>AM</small></div>
+  </div>
   <div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
     </div>
+    <div class="difference-bottom">Difference from current timezone: ${timeDifference} hours</div>
     <a href="/">All cities</a>
   `;
 }
@@ -68,3 +107,9 @@ setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
+// call the updateCity function in each city, passing it the timezone for that city
+// change the function to return the difference, not log it to the console
+// Put the returned figure into a sentence in the innerHTML for the default cities
+// Do the same for the selected city's innerHTML
+// Change the dublin code to local
